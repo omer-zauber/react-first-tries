83dad716f9648c057f5314d3dfcef75942b196ba
omer-zauber <omer.zauber@gmail.com>
2018-01-11 12:39:24 +0200

    started using the 'Class properties transform' babel plugin

--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,9 @@ module.exports = {
             test: /\.js$/,
             exclude: /node_module/
         }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: finalPath
     }
 };
